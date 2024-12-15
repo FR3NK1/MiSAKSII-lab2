006bbc7c7c939d26ac735387097e01809aa71ba6
Dmitriy Mironov <mironov_2001@mail.ru>
2024-12-15 20:49:33 +0300

    feat: Инициализировал react

---
+++ ui/eslint.config.js
@@ -0,0 +1,16 @@
+import pluginJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginReact from 'eslint-plugin-react'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  { 'react/react-in-jsx-scope': 'off' },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+]
